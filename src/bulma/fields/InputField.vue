7d92fc01073e6db47eaf9ff26634115326e4ffd3
mishu <mishu@xtelecom.ro>
2019-05-24 15:52:53 +0300

    adds v-model.number for numeric inputs

--- src/bulma/fields/InputField.vue
+++ src/bulma/fields/InputField.vue
@@ -1,11 +1,19 @@
 <template>
     <div :class="['control', { 'has-icons-right': errors.has(field.name) }]">
         <input :class="['input', { 'is-danger': errors.has(field.name) }]"
+            v-model.number="field.value"
+            :type="field.meta.content"
+            v-bind="{ ...field.meta, placeholder: i18n(field.meta.placeholder) }"
+            v-on="$listeners"
+            @input="errors.clear(field.name); $emit('changed')"
+            v-if="field.meta.content === 'number'">
+        <input :class="['input', { 'is-danger': errors.has(field.name) }]"
             v-model="field.value"
             :type="field.meta.content"
             v-bind="{ ...field.meta, placeholder: i18n(field.meta.placeholder) }"
             v-on="$listeners"
-            @input="errors.clear(field.name); $emit('changed')">
+            @input="errors.clear(field.name); $emit('changed')"
+            v-else>
         <error-icon v-if="errors.has(field.name)"/>
     </div>
 </template>
