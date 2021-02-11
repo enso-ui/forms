b0eb45727cc966847e1668407dad6585095fed88
Adrian Ocneanu <aocneanu@gmail.com>
2021-02-11 19:25:58 +0200

    Update FormSection.vue

--- src/bulma/parts/FormSection.vue
+++ src/bulma/parts/FormSection.vue
@@ -1,24 +1,23 @@
 <template>
-    <div>
-        <div class="section-divider"
-            v-if="section.divider">
-            <divider class="has-margin-bottom-medium"
-                :title="i18n(section.title)"
-                :placement="state.data.dividerTitlePlacement"/>
-        </div>
+    <div class="section-wrapper">
+        <divider class="mt-5 mb-5"
+            :title="i18n(section.title)"
+            :placement="state.data.dividerTitlePlacement"
+            v-if="section.divider"/>
         <div class="columns has-margin-bottom-large is-multiline"
-            v-for="(fields, index) in fieldChunks(section)"
-            :key="`chunk-${index}`">
-            <div v-for="field in fields"
-                class="column"
-                :class="section.columns === 'custom' && `is-${field.column}`"
-                :key="field.name">
-                <template v-if="field.dummy"/>
-                <slot :name="field.name"
-                    v-else-if="field.meta.custom"/>
-                <form-field :field="field"
-                    v-on="$listeners"
-                    v-else/>
+            v-for="row in rows"
+            :key="`${section.id}-${row}`">
+            <div class="column"
+                v-for="column in columns"
+                :class="cssClass(row, column)"
+                :key="index(row, column)">
+                <template v-if="index(row, column) < section.fields.length">
+                    <slot :name="section.fields[index(row, column)].name"
+                        v-if="section.fields[index(row, column)].meta.custom"/>
+                    <form-field :field="section.fields[index(row, column)]"
+                        v-on="$listeners"
+                        v-else/>
+                </template>
             </div>
         </div>
     </div>
@@ -33,7 +32,7 @@ export default {
 
     components: { FormField, Divider },
 
-    inject: ['state', 'i18n', 'fieldChunks'],
+    inject: ['state', 'i18n'],
 
     props: {
         section: {
@@ -41,8 +40,35 @@ export default {
             required: true,
         },
     },
+
+    computed: {
+        columns() {
+            const { columns, fields } = this.section;
+
+            return columns === 'custom' ? fields.length : columns;
+        },
+        rows() {
+            const { columns, fields } = this.section;
+
+            return columns === 'custom' ? 1 : Math.ceil(fields.length / columns);
+        },
+    },
+
+    methods: {
+        cssClass(row, column) {
+            const { columns, fields } = this.section;
+
+            return columns === 'custom'
+                ? `is-${fields[this.index(row, column)].column}`
+                : null;
+        },
+        index(row, column) {
+            return (row - 1) * this.columns + column - 1;
+        },
+    }
 };
 </script>
+
 <style>
 .section-divider {
     width: 100%;
