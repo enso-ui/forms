437e106ddf8abe5696f5e27c8b77e1a630e7dc51
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2024-09-27 13:25:12 +0300

    adds preview for wysiwyg fields to prevent unneccessary tinymce consumption

--- src/bulma/fields/WysiwygField.vue
+++ src/bulma/fields/WysiwygField.vue
@@ -2,29 +2,53 @@
     <wysiwyg v-bind="field.meta"
         v-model="field.value"
         :api-key="field.meta.apiKey"
-        :plugins="field.meta.plugins"
-        :toolbar="field.meta.toolbar"
         :has-error="errors.has(field.name)"
-        @update:model-value="errors.clear(field.name); $emit('changed')"/>
+        @update:model-value="errors.clear(field.name); emit('changed')"
+        v-if="editing"/>
+    <div class="wysiwyg-preview has-background-white p-1"
+        v-else>
+        <button class="button is-small is-outlined edit"
+            @click="editing = true">
+            {{ i18n('edit') }}
+        </button>
+        <div class="preview"
+            v-html="field.value"/>
+    </div>
 </template>
 
-<script>
+<script setup>
+
+import {
+    defineEmits, defineProps, inject, ref,
+} from 'vue';
 import Wysiwyg from '@enso-ui/wysiwyg/bulma';
 
-export default {
-    name: 'WysiwygField',
-
-    components: { Wysiwyg },
-
-    inject: ['errors'],
-
-    props: {
-        field: {
-            type: Object,
-            required: true,
-        },
+defineProps({
+    field: {
+        type: Object,
+        required: true,
     },
+});
 
-    emits: ['changed'],
-};
+const errors = inject('errors');
+const i18n = inject('i18n');
+
+const emit = defineEmits(['changed']);
+const editing = ref(false);
+
 </script>
+<style lang="scss">
+    .wysiwyg-preview {
+        max-height: 300px;
+        position: relative;
+        .button.edit {
+            position: absolute;
+            top: 0.3em;
+            right: 0.3em;
+        }
+        .preview {
+            max-height: 300px;
+            overflow-y: auto;
+        }
+    }
+</style>
